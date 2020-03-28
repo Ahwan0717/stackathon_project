d790a03ccfbe4f123e531e82d920b569688f693f
Ariana Hwang <ajhwang0717@gmail.com>
2020-03-28 13:00:58 -0400

    feature: added back app.js content

---
+++ src/App.js
@@ -0,0 +1,273 @@
+import React from 'react';
+import './App.css';
+import Spotify from 'spotify-web-api-js';
+import axios from 'axios'
+import styled from 'styled-components';
+
+const spotifyWebApi = new Spotify();
+
+const GridContainer = styled.div`
+	/* ... */
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+
+`
+
+const GridItem = styled.div`
+	/* ... */
+	display: flex;
+	flex-direction: column;
+
+	width: 200px;
+	height: 200px;
+
+	padding: 1rem;
+
+	img {
+		width: 100%;
+		height: 100%;
+	}
+
+	a {
+		display: block;
+		margin: 0;
+	}
+
+	div {
+		margin: 0;
+	}
+
+`
+const Title = styled.h1`
+/* ... */
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+
+`
+
+const Text1 = styled.h3`
+/* ... */
+font-size: 1.5em;
+text-align: center;
+`
+
+const Text2 = styled.p`
+/* ... */
+font-size: 1.5em;
+text-align: center;
+`
+
+
+class App extends React.Component {
+	constructor() {
+		super()
+		const params = this.getHashParams()
+
+		this.state = {
+			artistNames: ['Nobuo Uematsu', 'Akira Yamaoka', 'Andrew Hale', 'Koji Kondo', 'Norihiko Hibino', 'Yoko Shimomura'],
+			artistInfo: [],
+			topTracks: [],
+			loggedIn: params.access_token ? true : false,
+			nowPlaying: {
+				name: 'Not Checked',
+				image: ''
+			}
+		}
+		// Use to bind functions
+		if (params.access_token) {
+			spotifyWebApi.setAccessToken(params.access_token)
+		}
+	}
+
+	getHashParams = () => {
+		var hashParams = {};
+		var e, r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		while (e = r.exec(q)) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+		}
+		return hashParams;
+	}
+
+
+	// Get Basic Info
+
+	componentDidMount() {
+
+		// Loop through names array
+
+		for (let i = 0; i < this.state.artistNames.length; i++) {
+			let name = this.state.artistNames[i]
+			// Make a request for a user with a given ID
+			let artistInfoCopy = this.state.artistInfo; //copy of state
+			let topTracksCopy = this.state.topTracks;
+
+
+			let $this = this; //need to keep context of the this 
+
+			// Get Data based on each name to populate our state: artistInfo 
+			// axios get request
+			// Pass artist id into request
+			axios({
+				method: 'get',
+				url: `https://spotify-api-wrapper.appspot.com/artist/${name}`,
+			})
+				.then(function (response) {
+					// handle success
+					let obj = {}
+					// console.log('data', response);
+
+					// Save the ID, other information we might need
+					var id = response.data.artists.items[0].id;
+					var name = response.data.artists.items[0].name;
+
+					obj.name = name;
+					obj.id = id;
+
+					artistInfoCopy.push(obj)
+
+					$this.setState({
+						artistInfo: artistInfoCopy
+					})
+
+					// Top Tracks
+					// console.log('looping through artist info for id')
+					if ($this.state.artistInfo.length > 0) {
+						for (let i = 0; i < $this.state.artistInfo.length; i++) {
+							let id = $this.state.artistInfo[i].id;
+							// console.log('id', id)
+
+							axios({
+								method: 'get',
+								url: `https://spotify-api-wrapper.appspot.com/artist/${id}/top-tracks`
+							})
+								.then(function (response) {
+									var tempobj = {
+										id: '',
+										tracks: []
+									}
+
+									var tracks = response.data.tracks
+
+									tempobj.id = id
+									tempobj.tracks = tracks
+
+									topTracksCopy.push(tempobj)
+									// console.log('tempobj with data', tempobj)
+
+									$this.setState({
+										topTracks: topTracksCopy
+									});
+
+								})
+								.catch(function (error) {
+									// handle error
+									console.log(error);
+								})
+
+						}
+					}
+
+					// console.log('ARTIST INFO', this.state)
+				})
+				.catch(function (error) {
+					// handle error
+					console.log(error);
+				});
+		}
+
+	}
+
+
+	getNowPlaying = () => {
+		spotifyWebApi.getMyCurrentPlaybackState()
+			.then((response) => {
+				// console.log("response")
+				this.setState({
+					nowPlaying: {
+						name: response.item.name,
+						image: response.item.album.images[0].url
+					}
+				})
+			})
+	}
+
+
+
+	render() {
+		// console.log("state:", this.state)
+
+		// console.log('this.state.topTracks BEFORE', this.state.topTracks)
+
+		if (this.state.topTracks.length > 0) {
+			return (
+				<GridContainer>
+					<Title>
+						<div>
+							<h1>Video Game Music Library</h1>
+						</div>
+					</Title>
+
+					<Text1>
+						<div>
+							<h3>Created using the Spotify API</h3>
+						</div>
+					</Text1>
+
+					<Text2>
+						<div>
+							<p>Discover the artists behind some of your favorite video games. Scroll to the bottom to check what song is playing after clicking on the artist (Spotify login is required).</p>
+						</div>
+					</Text2>
+
+					{
+						this.state.topTracks.map(obj => {
+							// let tempId = obj.id
+							console.log("obj", obj);
+							let i = 0;
+							return obj.tracks.map(track => {
+								i++;
+								console.log('TRACK', i, track)
+								var imgUrl = track.album.images[0].url;
+								var name = track.artists[0].name;
+
+								return (
+									<GridItem>
+										<a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer"><p>{name}</p></a>
+										<div>
+											<img src={imgUrl} />
+										</div>
+									</GridItem>
+								)
+							})
+						})
+					}
+					<div>
+						<h3>Play one of these songs on Spotify to find out which song is playing!</h3>
+						<a href='http://localhost:8888'>
+							<button>Login with Spotify</button>
+						</a>
+						<div>Now Playing {this.state.nowPlaying.name}</div>
+						<div>
+							<img src={this.state.nowPlaying.image} style={{ width: 100 }} />
+						</div>
+						{this.state.loggedIn &&
+							<button onClick={() => this.getNowPlaying()}>
+								Check Now Playing
+							</button>
+						}
+					</div>
+				</GridContainer>
+
+			)
+		}
+		return (
+			<div>
+				Loading...
+			</div>
+		)
+	}
+}
+export default App;
